2487ac6ba4b31819cc4e162a74612882a74de966
yl9286 <yl9286@gmail.com>
2020-08-09 20:58:33 +0900

    Updates

---
+++ precache-manifest.fc2194194b1739587c862aa9624ac0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d0cc6d472981cc3bd546dae6fb7bb3",
+    "url": "/yurim_movie_app/index.html"
+  },
+  {
+    "revision": "0210f304fcf6372c8de2",
+    "url": "/yurim_movie_app/static/css/main.fb7042e6.chunk.css"
+  },
+  {
+    "revision": "631cde274a1ed8618de6",
+    "url": "/yurim_movie_app/static/js/2.5e6efdf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yurim_movie_app/static/js/2.5e6efdf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0210f304fcf6372c8de2",
+    "url": "/yurim_movie_app/static/js/main.6c491168.chunk.js"
+  },
+  {
+    "revision": "bc8b311d6f8c992f3bb4",
+    "url": "/yurim_movie_app/static/js/runtime-main.be36fe75.js"
+  }
+]);
